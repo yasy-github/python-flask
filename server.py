72689b6eeeffdb561eeb2d9958e0c142b1e84fd7
yasy-github <whangyasy@gmail.com>
2023-02-03 09:23:19 +0700

    Python Flask app

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def hello_world(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/')
+def index():
+    return 'Index Page'
